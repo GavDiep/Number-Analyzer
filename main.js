74a1db7c8f45b40d0ac4563e9500505f7305a9c4
Sealio <146213110+GavDiep@users.noreply.github.com>
2024-10-02 09:12:39 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// Number Analyzer
+
+// HTML Elements
+let numEl = document.getElementById('numInput');
+
+// Add Event Listener
+numEl.addEventListener('change', analyzeNumber);
+
+// Event Function
+function analyzeNumber() {
+    // Get Number from Input Element
+    let numInput = Number(numEl.value);
+
+    // Analyze Number and display results (Some are commented out so you can test your functions individually without errors coming up)
+    document.getElementById('sign').innerHTML = getSign(numInput);
+    // document.getElementById('even-odd').innerHTML = evenOrOdd(numInput);
+    // document.getElementById('multiple').innerHTML = multipleOf10(numInput);
+    // document.getElementById('digits').innerHTML = numDigits(numInput);
+
+    //**BONUS**//
+    //document.getElementById('prime').innerHTML = isPrime(numInput);
+}
+
+// Analyze Functions - Add your functions below. These should match the named functions above (e.g. getSign). When ready to test, uncomment the appropriate line in analyzeNumber before running.
+
+
